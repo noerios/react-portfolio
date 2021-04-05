f9f19349473a25d621d94f31379b723f35fc4f66
noerios <noerios@gmail.com>
2021-04-04 22:20:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+import NavBar from './Components/NavBar';
+import HomePage from "./Pages/HomePage"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <NavBar />
+      </div>
+      <div className="main-content">
+        <div className="content">
+          <HomePage />
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                     deverloper | marketer
+                    <span>Rolando Noe Rios</span>
+                </h1>
+                <p className="h-sub-text">
+                    more things about me. they all go here, and this is basically a paragraph
+                    but ill just type words here that will look like ismething really great, 
+                    done even care what it says for now.
+                </p>
+                <div className="icons">
+                     
+                </div>
+            </header>
+             
+        </div>
+    )
+}
+
+export default HomePage;

83a5e44eb09ef98837bf1e643201686fc6a45287
noerios <noerios@gmail.com>
2021-05-09 14:44:19 -0500

    portfolio data

---
+++ src/Components/allportfolios.js
@@ -0,0 +1,43 @@
+import ocio from '../img/portImages/OcioRanchDiscoball.jpg';
+import barber from '../img/portImages/BarberBookerScreen.png';
+import password from '../img/portImages/Password_Feature_art.gif'
+import rioslaw from '../img/portImages/RLR.Logo.jpeg';
+import scheduler from '../img/portImages/WorkdaySchedulerScreenshot.png';
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'Squarespace',
+        link1: 'www.ocioranch.com',
+        image: ocio,
+        title: 'Ocio Ranch'
+    },
+    {
+        id: 2,
+        category: 'Bootstrap',
+        link1: 'babar-shop.herokuapp.com/',
+        image: barber,
+        title: 'Barbershop Booker'
+    },
+    {
+        id: 3,
+        category: 'Javascript',
+        link1: 'noerios.github.io/homework3/',
+        image: password,
+        title: 'Password Generator'
+    },
+    {
+        id: 4,
+        category: 'Wix',
+        link1: 'www.rolandorioslaw.com',
+        image: rioslaw,
+        title: 'Rolando Rios Law'
+    },
+    {
+        id: 5,
+        category: 'Database',
+        link1: 'noerios.github.io/homework5/',
+        image: scheduler,
+        title: 'Workday Scheduler'
+    }
+]
